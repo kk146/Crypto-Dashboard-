a4f02dcb0faf8b981e9a1ad40318506fb207d17c
kk146 <kokilamishra1@gmail.com>
2026-07-08 02:06:32 +0530

    Create logo.jsx

---
+++ src/components/logo.jsx
@@ -0,0 +1,16 @@
+function Logo() {
+  return (
+    <div className="logo">
+      <div className="logo-icon">
+        <div className="cube"></div>
+      </div>
+
+      <div className="logo-text">
+        <h2>ALMA</h2>
+        <p>Better Together</p>
+      </div>
+    </div>
+  );
+}
+
+export default Logo;
