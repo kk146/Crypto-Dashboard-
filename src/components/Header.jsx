7e317e1820a039d2ca6e29d262d03048cda9ef8a
kk146 <kokilamishra1@gmail.com>
2026-07-10 01:00:23 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,38 +1,10 @@
-import logo from "../Almabetterlogo.png";
 import "../Styles/Header.css";
 
 function Header({ currency, setCurrency, search, setSearch }) {
   return (
     <header className="header">
 
-      {/* Left Side */}
-      <div className="header-left">
-
-        <div className="logo">
-          <img
-            src={logo}
-            alt="AlmaBetter"
-            className="logo-image"
-          />
-        </div>
-
-        <h2 className="logo-title">
-          AlmaBetter
-        </h2>
-
-      </div>
-
-      {/* Center Search */}
-      <div className="search-box">
-        <input
-          type="text"
-          placeholder="Search by coin..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        />
-      </div>
-
-      {/* Right Currency */}
+      {/* Currency Dropdown */}
       <div className="currency-box">
         <select
           value={currency}
@@ -43,6 +15,16 @@ function Header({ currency, setCurrency, search, setSearch }) {
           <option value="eur">EUR</option>
           <option value="gbp">GBP</option>
         </select>
+      </div>
+
+      {/* Search Box */}
+      <div className="search-box">
+        <input
+          type="text"
+          placeholder="Search by coin..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
 
     </header>
